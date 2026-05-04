d2358ec22c3ccabe5a5ff9491f3d7cbaf2416f8d
KrasnovDaniil1 <kd69931@gmail.com>
2026-05-04 22:26:58 +0200

    16

---
+++ 16_recursion.py
@@ -0,0 +1,40 @@
+# 1 лёгкая
+def F(n):
+    if n < 3: return 1
+    if n % 2 == 0: return F(n - 1) + n - 1
+    return F(n - 2) + 2* n - 2
+
+print(F(8))
+
+# 2 убираем лимит
+from sys import setrecursionlimit
+
+def F(n):
+    if n<3: return 3
+    return 2*n + 5 + F(n-2)
+
+setrecursionlimit(1600)
+print(F(3027) - F(3023))
+
+# 3 кэш
+from functools import lru_cache
+
+@lru_cache(None)
+def F(n):
+    if n<7: return 7
+    return n+1+F(n-2)
+
+for i in range(0, 2025):
+    F(i)
+
+print(F(2024) - F(2022))
+
+# сложное
+def G(n):
+    if n<10: return 2 * n
+    if n >= 10: return G(n-2) + 1
+
+def F(n):
+    return 2 * (G(n-3) + 8)
+
+print(F(15548))
