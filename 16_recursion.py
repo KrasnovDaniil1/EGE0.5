658e92d57f83e546d317616c4f8b682e6634234f
KrasnovDaniil1 <kd69931@gmail.com>
2026-05-05 10:00:40 +0200

    16

--- 16_recursion.py
+++ 16_recursion.py
@@ -6,7 +6,7 @@
 #
 # print(F(8))
 
-# 2 убираем лимит
+# 2 убираем лимит до 1000
 # from sys import setrecursionlimit
 #
 # def F(n):
@@ -30,18 +30,35 @@
 # print(F(2024) - F(2022))
 
 # сложное
+# from functools import lru_cache
+#
+# @lru_cache(None)
+# def G(n):
+#     if n<10: return 2 * n
+#     if n >= 10: return G(n-2) + 1
+#
+# @lru_cache(None)
+# def F(n):
+#     return 2 * (G(n-3) + 8)
+#
+# for i in range(0, 15548):
+#     F(i)
+#
+# print(F(15548))
+
 from functools import lru_cache
+
+
+def F(n):
+    if n >= 19: return F(n-4) + 3580
+    return 6 * (G(n-7) - 36)
 
 @lru_cache(None)
 def G(n):
-    if n<10: return 2 * n
-    if n >= 10: return G(n-2) + 1
+    if n >= 248045: return n/20 + 28
+    return G(n+9) - 4
 
-@lru_cache(None)
-def F(n):
-    return 2 * (G(n-3) + 8)
+for i in range(250000, 0, -1):
+    G(i)
 
-for i in range(0, 15548):
-    F(i)
-
-print(F(15548))
+print(F(673))
